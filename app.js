ed1f84ff8c6cc5807e493c2ed18e009a9ccff48a
sponge <spongenu@gmail.com>
2023-11-30 12:58:14 +0700

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/work-shift', function(req, res){
+  res.sendFile(__dirname + '/work-shift.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
